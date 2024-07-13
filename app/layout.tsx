91e82b3d29e0a24d9e6cf27b301b14f8d3352734
Jude Clarke <judepaclarke@gmail.com>
2024-07-13 17:37:30 -0600

    remove unused code, and prepare to go from styled components to css modules

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+import Chat from "./components/Chat/chat";
 
 export const metadata = {
   title: "Assistants API Quickstart",
@@ -13,8 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
+        <div className="centerChat">
+          <Chat />
+        </div>
       </body>
     </html>
   );
