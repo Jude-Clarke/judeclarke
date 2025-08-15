46505af31e9e86a9aec18ff9d101402cd1b0bad5
Jude Clarke <judepaclarke@gmail.com>
2025-08-15 13:59:35 -0400

    update skills exp

--- app/components/Skills/index.js
+++ app/components/Skills/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { skills } from '../../data/constants'
-import styles from "./index.module.css"
-import Image from 'next/image'
+import React from "react";
+import { skills } from "../../data/constants";
+import styles from "./index.module.css";
+import Image from "next/image";
+import yearsSince2020 from "@/app/utils/calcExp";
 
 const Skills = () => {
   return (
@@ -9,28 +10,42 @@ const Skills = () => {
       <div className={styles.wrapper}>
         <div className={styles.title}>Skills</div>
         <div className={styles.desc}>
-          Here are some skills I've been building over the past 5 years of my career.
+          Here are some skills I've been building over the past{" "}
+          {yearsSince2020()} years of my career.
         </div>
         <div className={styles["skills-container"]}>
           {skills.map((item, index) => (
             <div key={index} className={styles.skill}>
               <h2 className={styles["skill-title"]}>{item.title}</h2>
               <div className={styles["skill-list"]}>
-                {
-                  item.skills.map((skill, index) => (
-                    <a key={index} className={styles["skill-item"] + " " + (skill.minContent && "mobile-min-content")} href={skill.link || "javascript:void(0)"} target={`${skill.link.includes("http") ? "_blank" : "" }`}>
-                      <Image src={skill.image} alt={`${skill.name} logo`} className={styles["skill-image"]} width={24} height={24}/>
-                      {skill.name}
-                    </a>
-                  ))
-                }
+                {item.skills.map((skill, index) => (
+                  <a
+                    key={index}
+                    className={
+                      styles["skill-item"] +
+                      " " +
+                      (skill.minContent && "mobile-min-content")
+                    }
+                    href={skill.link || "javascript:void(0)"}
+                    target={`${skill.link.includes("http") ? "_blank" : ""}`}
+                  >
+                    <Image
+                      src={skill.image}
+                      alt={`${skill.name} logo`}
+                      className={styles["skill-image"]}
+                      width={24}
+                      height={24}
+                    />
+                    {skill.name}
+                  </a>
+                ))}
               </div>
             </div>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
