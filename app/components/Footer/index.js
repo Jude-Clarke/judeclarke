27bb02ca6d41304573fa519d016e7de20a428809
Jude Clarke <judepaclarke@gmail.com>
2026-01-01 11:32:14 -0500

    make bottom logo a link

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,6 +1,6 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Bio } from '../../data/constants';
-import styles from "./index.module.css"
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Bio } from "../../data/constants";
+import styles from "./index.module.css";
 
 const date = new Date().getFullYear();
 
@@ -8,21 +8,38 @@ function Footer() {
   return (
     <div className={styles.container}>
       <footer className={styles.wrapper}>
-        <h1 className={styles.logo}>Jude Clarke</h1>
+        <a className={styles.logo} href="#">
+          Jude Clarke
+        </a>
         <nav className={styles.nav}>
-          <a className={styles["nav-link"]} href="#about">About</a>
-          <a className={styles["nav-link"]} href="#skills">Skills</a>
-          <a className={styles["nav-link"]} href="#experience">Experience</a>
-          <a className={styles["nav-link"]} href="#projects">Projects</a>
-          <a className={styles["nav-link"]} href="#education">Education</a>
+          <a className={styles["nav-link"]} href="#about">
+            About
+          </a>
+          <a className={styles["nav-link"]} href="#skills">
+            Skills
+          </a>
+          <a className={styles["nav-link"]} href="#experience">
+            Experience
+          </a>
+          <a className={styles["nav-link"]} href="#projects">
+            Projects
+          </a>
+          <a className={styles["nav-link"]} href="#education">
+            Education
+          </a>
         </nav>
         <div className={styles["social-media-icons"]}>
-          <a className={styles["social-media-icon"]} href={Bio.linkedin} target="display"><LinkedInIcon /></a>
+          <a
+            className={styles["social-media-icon"]}
+            href={Bio.linkedin}
+            target="display"
+          >
+            <LinkedInIcon />
+          </a>
         </div>
         <p className={styles.copyright}>
           &copy; {date} Jude Clarke. All rights reserved.
         </p>
-
       </footer>
     </div>
   );
